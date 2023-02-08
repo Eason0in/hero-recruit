1154b3999bdf7b72258b867cad1d6f611f41e584
Eason <leoling127@gmail.com>
2023-02-08 21:17:43 +0800

    增加 router 讓頁面可以切換 route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import List from './Components/List'
+import Profile from './Components/Profile'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <h1>error</h1>,
+    children: [
+      {
+        path: 'heroes',
+        element: <List />,
+        children: [
+          {
+            path: ':heroId',
+            element: <Profile />,
+          },
+        ],
+      },
+    ],
+  },
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
