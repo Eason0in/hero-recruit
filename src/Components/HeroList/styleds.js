ecd2719f44933e31b05cf2ce0e69947d8de7717d
Eason <leoling127@gmail.com>
2023-02-11 10:32:20 +0800

    移除 寫在 HeroList 多餘的 CardList 樣式

--- src/Components/HeroList/styleds.js
+++ src/Components/HeroList/styleds.js
@@ -4,16 +4,6 @@ import { container } from '../../styles/_mixin'
 const Container = styled(container)`
   margin: 70px auto;
   box-sizing: border-box;
-
-  nav {
-    margin-bottom: 50px;
-    ul {
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      row-gap: 10px;
-    }
-  }
 `
 
 export { Container }
