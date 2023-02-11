79f7030e984fd61f29cf62d4e8e8504f7c136774
Eason <leoling127@gmail.com>
2023-02-11 10:31:24 +0800

    移除 test.js 檔案

--- src/Components/HeroList/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import HeroList from './index'
-
-test('renders learn react link', () => {
-  render(<HeroList />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
