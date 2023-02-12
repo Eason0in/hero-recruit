3db46810eec25fef12500c217f8657da7e8e3686
Eason <leoling127@gmail.com>
2023-02-12 16:07:06 +0800

    調整 heorList hover 卡片樣式的測試

--- cypress/e2e/heroList.cy.js
+++ cypress/e2e/heroList.cy.js
@@ -8,10 +8,12 @@ describe('Hero List 檢查', () => {
     cy.get('@listData').should('have.length', 4)
   })
 
-  it('hover 任一張卡片要有 active 樣式', () => {
-    cy.getByData('card-1').click().should('have.class', 'active')
-    cy.getByData('card-2').click().should('have.class', 'active')
-    cy.getByData('card-3').click().should('have.class', 'active')
-    cy.getByData('card-4').click().should('have.class', 'active')
+  it('hover 任一張卡片要會有字體顏色變化及 border 樣式', () => {
+    cy.get('@listData').each(({ id }) => {
+      cy.getByData(`card-${id}`)
+        .click({ force: true })
+        .should('have.css', 'color', 'rgb(0, 149, 144)')
+        .and('have.css', 'border-color', 'rgb(0, 149, 144)')
+    })
   })
 })
