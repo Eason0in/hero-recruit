7e0b10a4c6ad0f1e4a53d3d7d498ab980a0f772d
Eason <leoling127@gmail.com>
2023-02-12 16:09:21 +0800

    增加 heroProfile 的測試 cypress檔

---
+++ cypress/e2e/heroProfile.cy.js
@@ -0,0 +1,22 @@
+describe('Hero Profile 檢查 render', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.request('GET', 'https://hahow-recruit.herokuapp.com/heroes').its('body').as('listData')
+  })
+
+  it('點選的英雄 id 有出現在 url params 以及卡片有 active class', () => {
+    cy.get('@listData').each(({ id }) => {
+      cy.getByData(`card-${id}`).click().should('have.class', 'active')
+      cy.url().should('include', id)
+    })
+  })
+
+  it('點選任一張卡片要出現下方 dog、profile 和 remainPointAndBtn 區塊', () => {
+    cy.get('@listData').each(({ id }) => {
+      cy.getByData(`card-${id}`).click()
+      cy.getByData('dog-div').should('exist')
+      cy.getByData('profile-article').should('exist')
+      cy.getByData('profile-aside').should('exist')
+    })
+  })
+})
